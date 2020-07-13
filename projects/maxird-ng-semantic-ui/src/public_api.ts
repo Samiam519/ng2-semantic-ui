4d0d15707f0abd1ed0334decc62502f8242f267a
Sam Jimenez <samanthajimenez@maximus.com>
2020-07-13 11:26:52 -0600

    update name and version

--- projects/maxird-ng-semantic-ui/src/public_api.ts
+++ projects/maxird-ng-semantic-ui/src/public_api.ts
@@ -1,10 +1,10 @@
 /*
- * Public API Surface of sdm-ng-semantic-ui
+ * Public API Surface of maxird-ng-semantic-ui
  */
 
-// export * from './lib/sdm-ng-semantic-ui.service';
-// export * from './lib/sdm-ng-semantic-ui.component';
-// export * from './lib/sdm-ng-semantic-ui.module';
+// export * from './lib/maxird-ng-semantic-ui.service';
+// export * from './lib/maxird-ng-semantic-ui.component';
+// export * from './lib/maxird-ng-semantic-ui.module';
 
 
 // export { SuiModule } from './index';
